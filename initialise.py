abacf0bed7366ee16526444b1ffba37e37c805e0
Alexander Farquharson <adm.alexander.farquh@group.net>
2023-01-17 16:59:00 +0000

    af_import: changed for use on azure vm

--- initialise.py
+++ initialise.py
@@ -1,4 +1,4 @@
 import os, sys
 
-raw_path = "C:/Users/farqu/Documents/DS_Masters/DSMP/data/x01_raw"
-processed_path = "C:/Users/farqu/Documents/DS_Masters/DSMP/data/x02_processed"
+raw_path = "P:/Users/Alexander Farquharson/DSMP/x01_data/x01_raw"
+processed_path = "P:/Users/Alexander Farquharson/DSMP/x01_data/x02_processed"
